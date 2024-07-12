5a5238311291596635da8349cc7e96ecc437b528
Odayakana <aokromanton@gmail.com>
2024-07-12 23:11:37 +0300

    Create task_2.py

---
+++ task_2.py
@@ -0,0 +1,11 @@
+example = 'Lorem ipsum dolor sit amet'
+
+leng = len(example) // 2
+
+print(leng)
+
+print(example[0])
+print(example[-1])
+print(example[leng:])
+print(example[::-1])
+print(example[::2])
