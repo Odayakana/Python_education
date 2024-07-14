aeedc607a56a7a604adc27391030aa850582f709
Odayakana <aokromanton@gmail.com>
2024-07-14 18:31:09 +0300

    Create task_5.py

---
+++ task_5.py
@@ -0,0 +1,8 @@
+my_string = input('Произвольная строка: ')
+leng = len(my_string)
+print(leng)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
