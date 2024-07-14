195da1dc94fa48bbb8104ac4c25ebdbfe787b683
Odayakana <aokromanton@gmail.com>
2024-07-14 19:08:40 +0300

    Update homework_5.py

--- homework_5.py
+++ homework_5.py
@@ -1,6 +1,12 @@
 immutable_var = 1, True, 'string', 5.1
 print('Immutable tuple: ', immutable_var)
-# immutable_var[1] = False
+# immutable_var[1] = False - Неизменяемый элемент нельзя изменить:
+
+# Traceback (most recent call last):
+#   File "H:\python_projects\Hello\Python_education\homework_5.py", line 3, in <module>
+#     immutable_var[1] = False
+#     ~~~~~~~~~~~~~^^^
+# TypeError: 'tuple' object does not support item assignment
 
 mutable_list = [1, True, 'string', 5.1]
 mutable_list[2] = ('apple', 2)
