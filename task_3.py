65b509dee0dc909c9362416500f5dfcded036bcb
Odayakana <aokromanton@gmail.com>
2024-07-14 12:19:41 +0300

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,6 @@
+num_of_hw_completed = 12
+num_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = num_of_hours_spent / num_of_hw_completed
+
+print('Курс: ', course_name, ', всего задач: ', num_of_hw_completed, ', затрачено часов: ', num_of_hours_spent, ', среднее время выполнения ', time_per_task, ' часа.')
