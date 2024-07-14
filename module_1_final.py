4d1c612c5a81caddc84b6e3d8fb2536796225ee3
Odayakana <aokromanton@gmail.com>
2024-07-14 22:38:15 +0300

    Create module_1_final.py

---
+++ module_1_final.py
@@ -0,0 +1,13 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students_list = list(students)
+students_list.sort()
+
+total_grades_dict = {}
+
+for i in range(len(students_list)):
+  gade = sum(grades[i]) / len(grades[i])
+  total_grades_dict[students_list[i]] = gade
+
+print(total_grades_dict)
