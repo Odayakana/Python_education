f205187a9313a3bb622957fae9cc2e6177fa8ab6
Odayakana <aokromanton@gmail.com>
2024-07-30 05:16:07 +0300

    Create module_2_hard.py

---
+++ module_2/module_2_hard.py
@@ -0,0 +1,14 @@
+def get_pass(value):
+    result = ''
+    for i in range(1, value):
+        for j in range(i, value):
+
+            if value % (i + j) == 0 and j != i:
+                result += str(i) + str(j)
+
+    return result
+
+
+for i in range(1, 21):
+    result_pass = get_pass(i)
+    print(f'{i} - {result_pass}')
