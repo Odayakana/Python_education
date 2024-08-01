08cac36a1f25570f879e6766e699f86284ebae34
Odayakana <aokromanton@gmail.com>
2024-08-01 22:57:55 +0300

    Create module_3_3.py

---
+++ module_3/module_3_3.py
@@ -0,0 +1,15 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params(b=25)
+print_params(c=[1, 2, 3])
+
+values_list = [1, True, 'string']
+values_dict = {'a': False, 'b': [3, 2, 1], 'c': 3}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 42)
