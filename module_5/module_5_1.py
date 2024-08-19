03a5007cf9e360c66f8d0d7a5af35e971dda91e6
Odayakana <aokromanton@gmail.com>
2024-08-19 14:28:48 +0300

    Create module_5_1.py

---
+++ module_5/module_5_1.py
@@ -0,0 +1,20 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if self.number_of_floors < new_floor:
+            print ("Такого этажа не существует")
+
+        else:
+            floor = 1
+            while floor <= new_floor:
+                print(floor)
+                floor += 1
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
