a8c2eca714fc2d9de8128e0529cf83d3902e53d9
Odayakana <aokromanton@gmail.com>
2024-08-26 22:00:48 +0300

    Create module_5_3.py

---
+++ module_5/module_5_3.py
@@ -0,0 +1,99 @@
+class House():
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return 'Не объект House'
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return 'Не объект House'
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return 'Не объект House'
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return 'Не объект House'
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return 'Не объект House'
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return 'Не объект House'
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+            return self
+        return 'Не число'
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return 'Не число'
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = value + self.number_of_floors
+            return self
+        return 'Не число'
+
+    def __sub__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors - value
+            return self
+        return 'Не число'
+
+    def go_to(self, new_floor):
+        if self.number_of_floors < new_floor and new_floor >= 0:
+            print ("Такого этажа не существует")
+
+        else:
+            floor = 1
+            while floor <= new_floor:
+                print(floor)
+                floor += 1
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
