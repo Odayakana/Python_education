3742c254e9a609e260439e71f8b0877a69dbefb3
Odayakana <aokromanton@gmail.com>
2024-08-27 09:29:04 +0300

    Update module_5_3.py

--- module_5/module_5_3.py
+++ module_5/module_5_3.py
@@ -12,56 +12,52 @@ class House():
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
-        return 'Не объект House'
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        return 'Не является объектом House или числом'
 
     def __ne__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
-        return 'Не объект House'
+        return not self.__eq__(other)
 
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
-        return 'Не объект House'
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        return 'Не является объектом House или числом'
 
     def __le__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
-        return 'Не объект House'
+        return self.__eq__(other) or self.__lt__(other)
 
     def __gt__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
-        return 'Не объект House'
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        if isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
-        return 'Не объект House'
+        return not self.__lt__(other)
 
     def __add__(self, value):
         if isinstance(value, int):
             self.number_of_floors = self.number_of_floors + value
             return self
-        return 'Не число'
+        elif isinstance(value, House):
+            self.number_of_floors = self.number_of_floors + value.number_of_floors
+            return self
+        return 'Не является объектом House или числом'
 
     def __iadd__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors += value
-            return self
-        return 'Не число'
+        return self.__add__(value)
 
     def __radd__(self, value):
-        if isinstance(value, int):
-            self.number_of_floors = value + self.number_of_floors
-            return self
-        return 'Не число'
+        return self.__add__(value)
 
     def __sub__(self, value):
         if isinstance(value, int):
             self.number_of_floors = self.number_of_floors - value
             return self
-        return 'Не число'
+        elif isinstance(value, House):
+            self.number_of_floors = self.number_of_floors - value.number_of_floors
+            return self
+        return 'Не является объектом House или числом'
 
     def go_to(self, new_floor):
         if self.number_of_floors < new_floor and new_floor >= 0:
