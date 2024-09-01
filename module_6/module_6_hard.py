dc3eaaf69811724679e0384f3d60cefe7cb38234
Odayakana <aokromanton@gmail.com>
2024-09-02 00:17:52 +0300

    Update module_6_hard.py

--- module_6/module_6_hard.py
+++ module_6/module_6_hard.py
@@ -165,26 +165,26 @@ class Cube(Figure):
         canvas.create_text(330, 125, text=text_color, tags="tip_text", anchor=NW, **tip_font)
 
 
-# circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
-# cube1 = Cube((222, 35, 130), 6)
-#
-# # Проверка на изменение цветов:
-# circle1.set_color(55, 66, 77)  # Изменится
-# print(circle1.get_color())
-# cube1.set_color(300, 70, 15)  # Не изменится
-# print(cube1.get_color())
-#
-# # Проверка на изменение сторон:
-# cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
-# print(cube1.get_sides())
-# circle1.set_sides(15)  # Изменится
-# print(circle1.get_sides())
-#
-# # Проверка периметра (круга), это и есть длина:
-# print(len(circle1))
-#
-# # Проверка объёма (куба):
-# print(cube1.get_volume())
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+circle1.set_color(55, 66, 77)  # Изменится
+print(circle1.get_color())
+cube1.set_color(300, 70, 15)  # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+print(cube1.get_sides())
+circle1.set_sides(15)  # Изменится
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
 
 
 def convert_color(color):
